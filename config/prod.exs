33f055a88b66327d2fd3fae51cb3b2672c3ba2b8
Eksperimental <eksperimental@autistici.org>
2021-11-12 14:41:36 -0500

    Listen to port 4000 in prod env

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :maintenance_web, MaintenanceWeb.Endpoint,
-  url: [host: "localhost", port: 80],
+  url: [host: "localhost", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
