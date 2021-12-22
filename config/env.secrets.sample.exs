0339e04107ba495765c59c9ad42d60c0a8e8b3a4
Eksperimental <eksperimental@autistici.org>
2021-12-21 21:45:19 -0500

    Add config/env.secrets.sample.exs

---
+++ config/env.secrets.sample.exs
@@ -0,0 +1,20 @@
+# Rename this file: config/env.secrets.exs
+
+secret_key_base = "YOUR_SECRET_KEY_BASE"
+github_access_token = "YOUR_GITHUB_ACCESS_TOKEN"
+
+import Config
+
+config :maintenance_web, :secret_key_base, secret_key_base
+config :maintenance_web, :github_access_token, github_access_token
+
+path =
+  __DIR__
+  |> Path.join("..")
+  |> Path.join("env.secrets.sh")
+  |> Path.expand()
+
+File.write(path, """
+export SECRET_KEY_BASE="#{secret_key_base}"
+export GITHUB_ACCESS_TOKEN="#{github_access_token}"
+""")
