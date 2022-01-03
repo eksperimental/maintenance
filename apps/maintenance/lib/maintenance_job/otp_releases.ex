3fcad1d6df0f0407f229106ecee5be91d4868106
Eksperimental <eksperimental@autistici.org>
2022-01-03 05:56:14 -0500

    Update MaintenanceJob.OtpReleases

--- apps/maintenance/lib/maintenance_job/otp_releases.ex
+++ apps/maintenance/lib/maintenance_job/otp_releases.ex
@@ -5,6 +5,11 @@ defmodule MaintenanceJob.OtpReleases do
 
   @job :otp_releases
 
+  @erlang_download_url "https://erlang.org/download"
+  @github_tags_url "https://api.github.com/repos/erlang/otp/tags?per_page=100"
+  @version_table_url "https://raw.githubusercontent.com/erlang/otp/master/otp_versions.table"
+  @github_releases_url "https://api.github.com/repos/erlang/otp/releases?per_page=100"
+
   import Maintenance, only: [is_project: 1, info: 1]
   import Maintenance.Project, only: [config: 2]
   alias Maintenance.{Git, DB}
@@ -15,6 +20,13 @@ defmodule MaintenanceJob.OtpReleases do
   @type response :: %Finch.Response{}
   @type response_status :: pos_integer()
   @type contents :: %{required(file_name :: String.t()) => file_contents :: String.t()}
+
+  defmacrop debug(term) do
+    quote bind_quoted: [term: term, line: __CALLER__.line] do
+      IO.inspect({line, term}, limit: :infinity, printable_limit: :infinity)
+      term
+    end
+  end
 
   #############################
   # Callbacks
@@ -45,38 +57,28 @@ defmodule MaintenanceJob.OtpReleases do
       true ->
         fn_task_write_foo = fn ->
           {:ok, otp_versions_table} = get_versions_table()
-          IO.inspect({__ENV__.line, otp_versions_table})
+          # {:ok, otp_versions_table} = Data.get_versions_table()
 
           versions = parse_otp_versions_table(otp_versions_table)
-          IO.inspect({__ENV__.line, versions})
+          # debug(versions)
+          # debug(otp_versions_table)
 
           downloads = parse_erlang_org_downloads()
-          IO.inspect({__ENV__.line, downloads})
+          tags = parse_github_tags()
 
-          # tags = parse_github_tags()
-          # IO.inspect {__ENV__.line, tags}
+          {:ok, gh_releases} = gh_get(@github_releases_url)
+          # {:ok, gh_releases} = Data.gh_get()
 
-          {:ok, gHReleases} =
-            gh_get("https://api.github.com/repos/erlang/otp/releases?per_page=100")
+          releases =
+            for {major, patches} <- versions do
+              process_patches(major, patches, downloads, tags, gh_releases)
+            end
 
-          IO.inspect({__ENV__.line, gHReleases})
+          json_path = Path.join(Git.path(project), "priv/otp_releases.json")
+          info("Writting opt releases: #{json_path}")
 
-          # releases =
-          #   Map.map( versions, fn key, val ->
-          #     process_patches(key, val, downloads, tags, gHReleases)
-          #   end)
-
-          # IO.inspect {__ENV__.line, releases}
-
-          # :ok =
-          #   File.write(
-          #     Path.join(Maintenance.cache_path(), "otp_releases.json"),
-          #     create_release_json(releases)
-          #   )
-
-          # :ok
-
-          # info("Writting foo file. [#{project}]")
+          :ok = File.write(json_path, create_release_json(releases))
+          Git.add(project, json_path)
         end
 
         run_tasks(project, [fn_task_write_foo])
@@ -88,7 +90,7 @@ defmodule MaintenanceJob.OtpReleases do
   """
   @spec get_versions_table() :: {:ok, contents :: String.t()} | :error
   def get_versions_table() do
-    result = Req.get!("https://raw.githubusercontent.com/erlang/otp/master/otp_versions.table")
+    result = Req.get!(@version_table_url)
 
     case result do
       %{status: 200} ->
@@ -104,9 +106,7 @@ defmodule MaintenanceJob.OtpReleases do
   """
   @spec get_downloads() :: {:ok, contents :: String.t()} | :error
   def get_downloads() do
-    result =
-      "https://erlang.org/download"
-      |> Req.get!()
+    result = Req.get!(@erlang_download_url)
 
     case result do
       %{status: 200} ->
@@ -123,10 +123,18 @@ defmodule MaintenanceJob.OtpReleases do
   defp bool(term), do: not (!term)
 
   def needs_update?(_project) do
-    # case get_versions_table() do
-    #   {:ok, _contents} -> false
-    #   :error -> true
-    # end
+    # {:ok, otp_versions_table} = get_versions_table()
+
+    # latest_otp_version =
+    #   otp_versions_table
+    #   |> Enum.at(-1)
+    #   |> elem(1)
+    #   |> List.first()
+
+    # json_path = Path.join(Git.path(project), "priv/otp_releases.json")
+
+    # otp_releases = Jason.decode(json_path)
+
     true
   end
 
@@ -151,10 +159,10 @@ defmodule MaintenanceJob.OtpReleases do
     |> Stream.run()
 
     # Commit
-    :ok = Git.commit(project, "Add foo file")
+    :ok = Git.commit(project, "Update otp_releases.json")
 
     data = %{
-      title: "Add foo file",
+      title: "Update OTP releases",
       db_key: @job,
       db_value: true
     }
@@ -165,6 +173,7 @@ defmodule MaintenanceJob.OtpReleases do
           {:ok, :updated}
 
         {:error, _} = error ->
+          IO.inspect(data)
           IO.warn("Could not create PR, failed with: " <> inspect(error))
           error
       end
@@ -183,7 +192,7 @@ defmodule MaintenanceJob.OtpReleases do
     DB.get(project, job)
   end
 
-  defp parse_otp_versions_table(versions_table) do
+  def parse_otp_versions_table(versions_table) do
     lines =
       versions_table
       |> String.trim()
@@ -211,12 +220,9 @@ defmodule MaintenanceJob.OtpReleases do
     Enum.group_by(versions, fn {_k, v} -> v end, fn {k, _v} -> k end)
   end
 
-  defp parse_erlang_org_downloads() do
+  def parse_erlang_org_downloads() do
     {:ok, the_downloads} = get_downloads()
-    IO.inspect({__ENV__.line, the_downloads})
-
     downloads = Regex.scan(~r{<a href="([^"/]+)"}, the_downloads, capture: :all_but_first)
-    IO.inspect({__ENV__.line, downloads})
 
     regexes = %{
       readme: ~R{^(?:otp_src_|OTP-)(.*)\.(?:readme|README)$}s,
@@ -228,208 +234,121 @@ defmodule MaintenanceJob.OtpReleases do
       src: ~R{^otp_src_(.*)\.tar\.gz$}s
     }
 
-    Enum.reduce(
-      downloads,
-      %{},
-      fn [download], vsns ->
+    for [download] <- downloads, reduce: %{} do
+      vsns ->
         results =
-          Enum.reduce(
-            regexes,
-            [],
-            fn {_key, regex}, acc ->
+          for {key, regex} <- regexes, reduce: [] do
+            acc ->
               case Regex.run(regex, download, capture: :all_but_first) do
                 nil ->
                   acc
 
-                x ->
-                  [x | acc]
+                [vsn] ->
+                  [{vsn, key} | acc]
               end
-            end
-          )
+          end
 
-        case results do
-          ms ->
-            :lists.foldl(
-              fn {vsn, key}, map ->
-                info = :maps.get(vsn, map, %{})
-
-                Map.put(
-                  map,
-                  vsn,
-                  Map.put(
-                    info,
-                    key,
-                    :erlang.iolist_to_binary(['https://erlang.org/download/', download])
-                  )
-                )
-              end,
-              vsns,
-              ms
-            )
+        for {vsn, key} <- results, reduce: vsns do
+          map ->
+            info = Map.get(map, vsn, %{})
+            link = @erlang_download_url <> "/" <> download
+            value = Map.put(info, key, link)
+            Map.put(map, vsn, value)
         end
-      end
-    )
+    end
   end
 
-  defp parse_github_tags() do
-    {:ok, json} = gh_get("https://api.github.com/repos/erlang/otp/tags?per_page=100")
-
-    :maps.from_list(
-      for tag <- json do
-        tagName = :maps.get("name", tag)
-
-        case :re.run(tagName, 'OTP[-_](.*)', [{:capture, :all_but_first, :binary}]) do
-          {:match, [vsn]} ->
-            {vsn, {tagName, :maps.get("tarball_url", tag)}}
-
-          :nomatch ->
-            {tagName, {tagName, :maps.get("tarball_url", tag)}}
-        end
-      end
-    )
+  def process_patches(major, patches, downloads, tags, releases) do
+    new_patches = pmap(patches, &process_patch(&1, releases, downloads, tags))
+    complete_patches = Enum.filter(new_patches, &is_map_key(&1, :readme))
+    %{patches: complete_patches, latest: List.last(complete_patches), release: major}
   end
 
-  defp process_patches(major, patches, downloads, tags, releases) do
-    newPatches =
-      pmap(
-        fn patch ->
-          process_patch(patch, releases, downloads, tags)
-        end,
-        patches
-      )
+  def process_patch(patch_vsn, releases, downloads, tags) do
+    erlang_org_download = Map.get(downloads, patch_vsn, %{})
 
-    completePatches =
-      :lists.filter(
-        fn patch ->
-          :maps.is_key(:readme, patch)
-        end,
-        newPatches
-      )
+    case Enum.find(releases, fn release ->
+           charlist_equal?(Map.get(release, "tag_name"), 'OTP-' ++ patch_vsn)
+         end) do
+      nil ->
+        {tag_name, src} = Map.get(tags, patch_vsn, {nil, nil})
+        Map.merge(erlang_org_download, %{tag_name: tag_name, src: src, name: patch_vsn})
 
-    %{patches: completePatches, latest: hd(completePatches), release: major}
-  end
-
-  defp process_patch(patchVsn, releases, downloads, tags) do
-    erlangOrgDownload = :maps.get(patchVsn, downloads, %{})
-
-    case :lists.search(
-           fn release ->
-             :string.equal(:maps.get("tag_name", release), 'OTP-' ++ patchVsn)
-           end,
-           releases
-         ) do
-      {:value, json} ->
-        assets = fetch_assets(:maps.get("assets", json))
+      json ->
+        assets = fetch_assets(Map.get(json, "assets"))
 
         patch = %{
-          name: patchVsn,
-          tag_name: :maps.get("tag_name", json),
-          published_at: :maps.get("published_at", json),
-          html_url: :maps.get("html_url", json)
+          name: patch_vsn,
+          tag_name: Map.get(json, "tag_name"),
+          published_at: Map.get(json, "published_at"),
+          html_url: Map.get(json, "html_url")
         }
 
-        :maps.merge(
-          erlangOrgDownload,
-          :maps.merge(patch, assets)
-        )
-
-      false ->
-        {tagName, src} = :maps.get(patchVsn, tags, {:undefined, :undefined})
-
-        :maps.merge(
-          %{tag_name: tagName, src: src, name: patchVsn},
-          erlangOrgDownload
-        )
+        Map.merge(erlang_org_download, Map.merge(patch, assets))
     end
+  end
+
+  defp charlist_equal?(charlist1, charlist2) do
+    :string.equal(charlist1, charlist2)
   end
 
   defp fetch_assets(assets) do
     matches = %{
-      readme: '^OTP-.*\\.README$',
-      html: '^otp_doc_html.*',
-      man: '^otp_doc_man.*',
-      win32: '^otp_win32.*',
-      win64: '^otp_win64.*',
-      src: '^otp_src.*'
+      readme: ~R{^OTP-.*\\.README$},
+      html: ~R{^otp_doc_html.*},
+      man: ~R{^otp_doc_man.*},
+      win32: ~R{^otp_win32.*},
+      win64: ~R{^otp_win64.*},
+      src: ~R{^otp_src.*}
     }
 
-    :maps.from_list(
-      :lists.flatmap(
-        fn {key, match} ->
-          case :lists.search(
-                 fn asset ->
-                   :re.run(
-                     :maps.get(
-                       "name",
-                       asset
-                     ),
-                     match
-                   ) !== :nomatch
-                 end,
-                 assets
-               ) do
-            {:value, v} ->
-              [{key, %{url: :maps.get("browser_download_url", v), id: :maps.get("id", v)}}]
+    Enum.flat_map(
+      Map.to_list(matches),
+      fn {key, match} ->
+        case Enum.find(assets, fn asset -> Regex.match?(match, Map.get(asset, "name")) end) do
+          nil ->
+            []
 
-            false ->
-              []
-          end
-        end,
-        :maps.to_list(matches)
-      )
+          value ->
+            [{key, %{url: Map.get(value, "browser_download_url"), id: Map.get(value, "id")}}]
+        end
+      end
     )
+    |> Enum.into(%{})
   end
 
   defp create_release_json(releases) do
-    Jason.encode(
-      Enum.map(
-        Map.to_list(releases),
-        fn {_Key, release} ->
-          Map.merge(release, %{
-            latest: strip_ids(Map.get(release, :latest)),
-            patches:
-              for patch <- Map.get(release, :patches) do
-                strip_ids(patch)
-              end
-          })
-        end
-      )
+    Jason.encode!(
+      for release <- releases do
+        Map.merge(release, %{
+          latest: strip_ids(release[:latest]),
+          patches:
+            for patch <- release[:patches] do
+              strip_ids(patch)
+            end
+        })
+      end,
+      pretty: true
     )
   end
 
   defp strip_ids(patch) do
-    Map.map(
-      patch,
-      fn
-        _Key, %{id: _, url: url} ->
-          url
+    Enum.filter(patch, fn
+      {_Key, %{id: _, url: url}} ->
+        url
 
-        _, value ->
-          value
-      end
-    )
+      {_, value} ->
+        value
+    end)
+    |> Enum.into(%{})
   end
 
-  defp pmap(fun, list) do
-    refs =
-      for a <- list do
-        {_, ref} =
-          spawn_monitor(fn ->
-            exit(fun.(a))
-          end)
-
-        ref
-      end
-
-    for ref <- refs do
-      receive do
-        {:DOWN, ^ref, _, _, value} ->
-          value
-      end
-    end
+  defp pmap(collection, func) do
+    collection
+    |> Enum.map(&Task.async(fn -> func.(&1) end))
+    |> Enum.map(&Task.await/1)
   end
 
-  # TODO: Implement this function
   defp gh_get(url) do
     request_headers = [
       {"Accept", "application/vnd.github.v3+json"},
@@ -439,11 +358,8 @@ defmodule MaintenanceJob.OtpReleases do
     response = Req.get!(url, headers: request_headers)
 
     case response do
-      %{status: 200} ->
-        get_link(response)
-
-      _ ->
-        :error
+      %{status: 200} -> get_link(response)
+      _ -> :error
     end
   end
 
@@ -454,13 +370,29 @@ defmodule MaintenanceJob.OtpReleases do
         case Regex.run(~r/<([^>]+)>; rel="next"/, link, capture: :all_but_first) do
           nil ->
             body = Map.fetch!(response, :body)
-            {:ok, Jason.decode!(body)}
+            {:ok, body}
 
           [next_url] ->
             {:ok, next_json} = gh_get(next_url)
             body = Map.fetch!(response, :body)
-            {:ok, Jason.decode!(body) ++ next_json}
+            {:ok, body ++ next_json}
         end
+    end
+  end
+
+  def parse_github_tags() do
+    {:ok, json} = gh_get(@github_tags_url)
+
+    for tag <- json, into: %{} do
+      tag_name = Map.get(tag, "name")
+
+      case Regex.run(~r/OTP[-_](.*)/, tag_name, capture: :all_but_first) do
+        nil ->
+          {tag_name, {tag_name, Map.get(tag, "tarball_url")}}
+
+        [vsn] ->
+          {vsn, {tag_name, Map.get(tag, "tarball_url")}}
+      end
     end
   end
 end
